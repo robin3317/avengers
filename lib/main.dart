70de390db9fd027b383e93c3dded7643c3bd3d9b
robin3317 <abdurrahmanrobin47@gmail.com>
2019-11-01 21:21:34 +0600

    Teming done

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,30 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
+        primaryColor: Colors.red,
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+              ),
+              title: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+              body1: TextStyle(
+                fontSize: 23,
+                color: Colors.green,
+              ),
+            ),
+        appBarTheme: AppBarTheme(
+          textTheme: ThemeData.light().textTheme.copyWith(
+                title: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+        ),
       ),
       home: MyHomePage(title: 'Avengers'),
     );
@@ -31,6 +54,7 @@ class MyHomePage extends StatelessWidget {
           children: <Widget>[
             Text(
               'You have pushed the button this many times:',
+              style: Theme.of(context).textTheme.body1,
             ),
           ],
         ),
